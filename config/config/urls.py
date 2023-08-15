50305904b90014cea7fa0f13a1887ca4291f04a0
kym8821 <louie9798@naver.com>
2023-08-16 00:05:15 +0900

    api 명세서 오류 해결

--- config/config/urls.py
+++ config/config/urls.py
@@ -22,9 +22,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     # swagger 세팅
-    re_path(r'(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
+    re_path(r'^(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     # 실제 사용하는 URL
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
