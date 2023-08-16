a0d2ca347ce7afb045675af7cfa3577b06154306
Won-yeji <wyj3326@naver.com>
2023-08-16 20:00:57 +0900

    좋아요기능

--- config/column/urls.py
+++ config/column/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', ColumnListView.as_view(), name='column-list'),
     path('<uuid:column_id>/', ColumnCertainView.as_view(), name='column-detail'),
     path('register/', ColumnCRUDView.as_view(), name='column-register'), # 이거 자동으로 칼럼 현재 사용자한테 넣어줌
-    path('<uuid:column_id>/likes/<uuid:uuid>/', CheckPreferView.as_view(), name='check-like'),
     path('<uuid:column_id>/likes/', PreferView.as_view(), name='like'),
     path('search/', ColumnSearchListView.as_view(), name='column-search'),
 ]
